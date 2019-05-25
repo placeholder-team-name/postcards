43a752524fd67c5d36db56058093be16536320c7
William Kwok <wkwok16@uw.edu>
2019-05-24 17:24:40 -0700

    Draft to HTML and vice versa imported. Base for sending to firebase established

--- frontend/src/components/WriteAndPreviewComponent/WriteComponent/index.js
+++ frontend/src/components/WriteAndPreviewComponent/WriteComponent/index.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import draftToHtml from 'draftjs-to-html';
+import { convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { Button } from '../../globals';
 
 export const WriteComponent = ({ user, userNotebookContent, setUserNotebookContent }) => {
     const [savedLatestDataToFirebase, setSavedLatestDateToFirebase] = useState(true);
     const [errorSaving, setErrorSaving] = useState("");
 
     const sendUserNotebookContentToFirebase = () => {
+        let convertedToHtml = draftToHtml(convertToRaw(userNotebookContent.getCurrentContent()));
+        // console.log(convertedToHtml);
         // convert to HTML
         // send to firebase (uid/year/month)
         // 
@@ -15,5 +20,8 @@ export const WriteComponent = ({ user, userNotebookContent, setUserNotebookConte
     return <>
         <Editor editorState={userNotebookContent}
             onEditorStateChange={(e) => setUserNotebookContent(e)} />
+        <Button onClick={(e) => {
+            sendUserNotebookContentToFirebase();
+        }}>Test</Button>
     </>
 }

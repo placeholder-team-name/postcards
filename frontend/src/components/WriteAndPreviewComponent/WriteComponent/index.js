aa51cbb2540a2aed140adce2d7749767e0bc355a
William Kwok <wkwok16@uw.edu>
2019-05-28 16:03:10 -0700

    Updated writecomponent

--- frontend/src/components/WriteAndPreviewComponent/WriteComponent/index.js
+++ frontend/src/components/WriteAndPreviewComponent/WriteComponent/index.js
@@ -4,17 +4,26 @@ import { convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { Button } from '../../globals';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 export const WriteComponent = ({ user, userNotebookContent, setUserNotebookContent }) => {
     const [savedLatestDataToFirebase, setSavedLatestDateToFirebase] = useState(true);
     const [errorSaving, setErrorSaving] = useState("");
 
-    const sendUserNotebookContentToFirebase = () => {
-        let convertedToHtml = draftToHtml(convertToRaw(userNotebookContent.getCurrentContent()));
-        // console.log(convertedToHtml);
-        // convert to HTML
-        // send to firebase (uid/year/month)
-        // 
+    const sendUserNotebookContentToFirebase = async () => {
+        try {
+            let convertedToHtml = draftToHtml(convertToRaw(userNotebookContent.getCurrentContent()));
+            const currentTime = new Date();
+            const year = currentTime.getYear() + 1900;
+            const month = currentTime.getMonth();
+            const notebookRef = firebase.database().ref(`${user.uid}/${year}/${month}`);
+            await notebookRef.set(convertedToHtml);
+            setErrorSaving("");
+        }
+        catch (e) {
+            setErrorSaving(e.message);
+        }
     }
 
     return <>

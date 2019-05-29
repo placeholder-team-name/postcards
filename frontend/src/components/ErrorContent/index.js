2500096770764a98985c74eb96bd992ce7f19e6e
William Kwok <wkwok16@uw.edu>
2019-05-28 21:46:22 -0700

    Updated styles on error content

--- frontend/src/components/ErrorContent/index.js
+++ frontend/src/components/ErrorContent/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ErrorContent = ({ errorMessage }) => {
     return <>
         <div style={{
-            height: "50%", // TODO: set this to pixels or something to make it take up height
-            width: "100%",
+            height: 30, // TODO: set this to pixels or something to make it take up height
+            width: "80%",
             border: errorMessage ? "1px solid red" : "none"
         }}>
             {errorMessage && <>{errorMessage}</>}

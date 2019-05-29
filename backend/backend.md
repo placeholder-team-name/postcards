5a265e9f8b578561283111c85f429c19560b6ed2
Hyoju Claire Kim <hjkim97@outlook.com>
2019-05-28 17:58:08 -0700

    Finish implemeting /sendemail backend

---
+++ backend/backend.md
@@ -0,0 +1,33 @@
+## Endpoints
+
+### POST */SendEmail*
+Takes in request body: 
+
+    { 
+        "recipients": [ {"email":"example@email.com", "name": "Example Recipient"}, 
+                        {"email":"claireiscool@email.com", "name": "Claire"}]
+        "user": { 
+            "email": "user@email.com", 
+            "name": "John Doe" 
+        },
+        "html": "<h1>title of email</h1><p>some postcard content</p>"
+    }
+
+Returns:
+- Successful
+    - `{status: 200, message: "Email sent."}`
+- If recipient is empty
+    - `{status: 400, message: "Recipients have to contain one or more addresses."}`
+- if (req.body == null || req.body == undefined) 
+    - `{status: 400, message: "Request body cannot be empty"}`
+- Sendgrid catches error (example):
+
+        {
+            status: 400, 
+            message: Bad Request,
+            errors: { errors: [
+                { message: 'The to array is required for all personalization objects, and must have at least one email object with a valid email address.',
+            field: 'personalizations.0.to',
+            help:'http://sendgrid.com/docs/API_Reference/Web_API_v3/Mail/errors.html#message.personalizations.to' } 
+            ]}
+        }
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,59 @@
 const express = require('express');
 const cors = require('cors');
+var bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post('/SendEmail', function (req, res) {
-    let recipients = req.body.recepients;
-    let user = req.body.user;
-
-    if (len(recipients) < 1) {
-        res.status('400').json('Recipients has to contain one or more addresses.');
+    if (req.body == null || req.body == undefined) {
+        return res.json({
+            status: 400,
+            message: 'Request body cannot be empty'
+        });
     }
-    const msg = {
-        to: recipients,
-        from: user.email,
+    let recipients = req.body.recipients;
+    let user = req.body.user;
+    
+    if (recipients.length < 1) {
+        return rres.json({
+            status: 400,
+            message:'Recipients have to contain one or more addresses.'
+        });
+    }
+    const msg = {   
+        personalizations: [],
         subject: 'Postcards from ' + user.name,
         text: 'This was my month!', // change this text
         html: req.body.html,
+        from: user
       };
-      sgMail.sendMultiple(msg).then(() => {
-          res.status('200').json('Emails sent.');
-      })
-      .catch(error => {
-        //Extract error msg
-        const {message, code, response} = error;
-    
-        //Extract response msg
-        const {headers, body} = response;
+    recipients.map(recipient => {
+        msg.personalizations.push({to: [recipient]});
+    });
+        
+    sgMail.sendMultiple(msg).then(() => {
+        return res.json({
+            status: 200,
+            message: 'Emails sent.'
+        });
+    })
+    .catch(error => {
+    //Extract error msg
+    const {message, code, response} = error;
+    //Extract response msg
+    const {headers, body} = response;
 
-        console.log(headers);
-        console.log(body);
-        console.log(message + " "  + code + " " + response);
-        res.status(code).json(message);
-      });
+    return res.json({
+        status: code,
+        message: message,
+        errors: body
+    });
+    });
 });
 
 app.listen(3000, () => 
